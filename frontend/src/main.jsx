477c5b6cd74b872c55d2098a52088a6e22c32073
Leelamani sahu <leelamanisahu@gmal.com>
2024-11-10 19:28:44 +0530

    error page added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,54 +1,80 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import {Provider} from "react-redux";
-import { persistor, store } from './redux/store.js'
+import { StrictMode, Suspense, lazy } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react';
 
-import Home from './components/Home.jsx'
-import Auth from './components/Auth.jsx'
-import VideoPage from './components/VideoPage.jsx'
-import Channel from './components/Channel.jsx'
-import SearchResult from './components/SearchResult.jsx'
-
-
+// Lazy loading components
+const Home = lazy(() => import('./components/Home.jsx'));
+const Auth = lazy(() => import('./components/Auth.jsx'));
+const VideoPage = lazy(() => import('./components/VideoPage.jsx'));
+const Channel = lazy(() => import('./components/Channel.jsx'));
+const SearchResult = lazy(() => import('./components/SearchResult.jsx'));
+const ErrorPage = lazy(() => import('./components/ErrorPage.jsx')); 
 
 const appRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<App/>,
-    children:[
+    path: '/',
+    element: <App />,
+    children: [
       {
-        path:"/",
-        element:<Home/>
+        path: '/',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        ),
       },
       {
-        path:"/auth",
-        element:<Auth/>
+        path: '/auth',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Auth />
+          </Suspense>
+        ),
       },
       {
-        path:"/video/:id",
-        element:<VideoPage/>
+        path: '/video/:id',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <VideoPage />
+          </Suspense>
+        ),
       },
       {
-        path:"/channel/:channelId",
-        element:<Channel/>
+        path: '/channel/:channelId',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <Channel />
+          </Suspense>
+        ),
       },
       {
-        path:"/search/:searchQuery",
-        element:<SearchResult/>
-      }
-     
-    ]
-  }
-])
+        path: '/search/:searchQuery',
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
+            <SearchResult />
+          </Suspense>
+        ),
+      },
+    ],
+    errorElement:(
+      <Suspense fallback={<div>Loading...</div>}>
+      <ErrorPage />
+    </Suspense>
+    )
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-    <RouterProvider router={appRouter}/>
-    </PersistGate>
-  </Provider>
-)
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={appRouter} />
+      </PersistGate>
+    </Provider>
+  </StrictMode>
+);
